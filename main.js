504f587cbcb525b169c9c92f9d670d413e6daece
Hai.fe <hai.fe@agiletech.vn>
2021-04-10 12:42:36 +0700

    add port

--- main.js
+++ main.js
@@ -49,6 +49,6 @@ router.render = (req, res) => {
 
 // Use default router
 server.use('/api', router);
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
